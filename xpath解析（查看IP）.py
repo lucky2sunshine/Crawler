3d81fb98525f5afbc49f99daa79e677293b13821
lucky2sunshine <865021978@qq.com>
2020-12-01 20:37:56 +0800

    增加了爬取第一PPT的练习代码

--- xpath解析（查看IP）.py
+++ xpath解析（查看IP）.py
@@ -8,12 +8,12 @@ import time
 
 url = 'https://202020.ip138.com/'
 proxies = {
-    'https': 'https://113.124.92.134:9999'
+    'https': '123.163.121.169:9999'
 }
 headers = {
     'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'
 }
-page_text = requests.get(url, headers = headers, proxies = proxies, ).text
+page_text = requests.get(url, headers = headers, proxies = proxies ).text
 
 tree = etree.HTML(page_text)
 
